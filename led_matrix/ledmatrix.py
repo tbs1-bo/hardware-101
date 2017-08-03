c626c00da02eb161a60cee994898f1c3ceb87b20
Marco Bakera <marco@bakera.de>
2017-08-03 11:01:53 +0200

    added connect method for more explicit interface of LED matrix class.

--- led_matrix/ledmatrix.py
+++ led_matrix/ledmatrix.py
@@ -1,15 +1,15 @@
 """
-This module shows how to use an 5x7 led matrix.
+This module shows how to use an 5x7 led matrix. It contains a class LedMatrix
+that can be used to easily access a matrix.
 
-Pinout of the chip:
-  
-  ooooo
-1 ooooo 12
-2 ooooo 11
-3 ooooo 10
-4 ooooo 9
-5 ooooo 8
-6 ooooo 7
+Pinout of the matrix:
+
+1 +---+ 12
+2 |   | 11
+3 |   | 10
+4 |   | 9
+5 |   | 8
+6 +---+ 7
 
 Connections: Electrical current travels from columns to rows.
 
@@ -31,21 +31,28 @@ import time
 
 class LedMatrix:
     
-    def __init__(self, led_gpio, boardmode=GPIO.BCM):
-        """Create an led matrix with a mapping that maps led-matrix pins to
-        gpio pins."""
-        
-        self.led_gpio = led_gpio
-        
+    def __init__(self, boardmode=GPIO.BCM):
+        """Create an led matrix. Use the connect-method afterwards to connect
+        led matrix pins with GPIO pins on the Raspberry Pi."""
+
+        # a dictionary that holds led pins of the matrix and corresponding
+        # GPIO pin on the Raspberry Pi.
+        self.led_gpio = {}
+
+        # default pins on a 5x7 matrix
         self.x_pins = [1, 3, 10, 7, 8]
         self.y_pins = [12, 11, 2, 9, 4, 5, 6]
         
-        # using BCM numbering scheme
+        # using the given numbering scheme
         GPIO.setmode(boardmode)
 
+    def connect_pins(self, ledpin, gpio):
+        """Connect a Pin of the LED Matrix with a GPIO pin on the Raspberry
+        Pi."""
+        self.led_gpio[ledpin] = gpio
+
         # configure all pins as output
-        for pin in led_gpio:
-            GPIO.setup(led_gpio[pin], GPIO.OUT)
+        GPIO.setup(gpio, GPIO.OUT)
 
     def led(self, x, y, on_off):
         """Turn the led at coordinate (x,y) on or off. Starting with (0,0) at
@@ -53,27 +60,30 @@ class LedMatrix:
         handled this way. Use multiplexing instead - as described here
         https://www.mikrocontroller.net/articles/LED-Matrix#Multiplexbetrieb"""
 
-        # electric current travels from column (y) to row (x) if turned on
-        pin_lo = self.x_pins[x]
-        pin_hi = self.y_pins[y]
+        # electric current can only travel from column (y) to row (x)
+        if on_off:
+            ledpin_lo = self.x_pins[x]
+            ledpin_hi = self.y_pins[y]
+        else:
+            ledpin_hi = self.x_pins[x]
+            ledpin_lo = self.y_pins[y]
 
-        if not on_off:
-            # swap if necessary
-            pin_hi, pin_lo = pin_lo, pin_hi
-
-        # determine GPIO for led pins
-        gpio_hi, gpio_lo = self.led_gpio[pin_hi], self.led_gpio[pin_lo]
+        # determine GPIO pins for led pins
+        gpio_hi, gpio_lo = self.led_gpio[ledpin_hi], self.led_gpio[ledpin_lo]
 
         GPIO.output(gpio_hi, True)
         GPIO.output(gpio_lo, False)
 
 
 def main():
-    # mapping some pins of the led-matrix to GPIO-pins
-    ledpin_gpio = {1: 17, 3: 18, 11: 23, 12: 22}
-    ledmat = LedMatrix(ledpin_gpio)
+    ledmat = LedMatrix()
+    # connect pins of the led-matrix to GPIO-pins
+    ledmat.connect_pins(1, 17)
+    ledmat.connect_pins(3, 18)
+    ledmat.connect_pins(11, 23)
+    ledmat.connect_pins(12, 22)
 
-    # turning on each led from top left to bottom right - for 5 seconds
+    # turn on each of the 4 LEDs from top left to bottom right - for 5 seconds
     start = time.time()
     while time.time() - start < 5:
         for y in (0, 1):
@@ -83,7 +93,7 @@ def main():
                 ledmat.led(x, y, False)
                 time.sleep(0.2)
 
-    # turning on two leds at 0|0 and 0|1 using muliplexing
+    # turning on two leds at (0,0) and (0,1) using multiplexing
     while True:
         for x in (0, 1):
             for y in (0, 1):
