3dacf83053c10dcbaa1eced2ca278f4b139ede01
Marco Bakera <bakera@tbs1.de>
2017-08-03 06:42:08 +0000

    added class LedMatrix.

--- led_matrix/ledmatrix.py
+++ led_matrix/ledmatrix.py
@@ -29,26 +29,52 @@ import RPi.GPIO as GPIO
 import time
 
 
+class LedMatrix:
+    
+    def __init__(self, led_gpio, boardmode=GPIO.BCM):
+        """Create an led matrix with a configuration that maps led pins to gpio pins."""
+        self.led_gpio = led_gpio
+        
+        self.x_pins = [1, 3, 10, 7, 8]
+        self.y_pins = [12, 11, 2, 9, 4, 5, 6]
+        
+        # using BCM numbering scheme
+        GPIO.setmode(boardmode)
+
+        # configure all pins as output
+        for pin in led_gpio:
+            GPIO.setup(led_gpio[pin], GPIO.OUT)
+
+    def led(self, x, y, on_off):
+        """Turn the led at coordinate (x,y) on or off. (0,0) is a top left."""
+
+        pin_hi = self.x_pins[x]
+        pin_lo = self.y_pins[y]
+
+        if on_off:
+            # swap if necessary
+            pin_hi, pin_lo = pin_lo, pin_hi
+
+        gpio_hi, gpio_lo = self.led_gpio[pin_hi], self.led_gpio[pin_lo]
+
+        GPIO.output(gpio_hi, True)
+        GPIO.output(gpio_lo, False)
+
+
 def main():
     # mapping pins of the chip to GPIO-pins
     led_gpio = {1: 17, 3: 18, 11: 23, 12: 22}
-
-    # using BCM numbering scheme
-    GPIO.setmode(GPIO.BCM)
-
-    # configure all pins as output
-    for pin in led_gpio:
-        GPIO.setup(led_gpio[pin], GPIO.OUT)
+    ledm = LedMatrix(led_gpio)
 
     # turning on each led from top left to bottom right
-    while True:
+    while False and True:
         for x in (0, 1):
             for y in (0, 1):
-                led(x, y, True, led_gpio)
-                time.sleep(1)
-                led(x, y, False, led_gpio)
-                time.sleep(1)
-        
+                ledm.led(x, y, True)
+                time.sleep(0.2)
+                ledm.led(x, y, False)
+                time.sleep(0.2)
+
 
 def led(x, y, on_off, led_gpio):
     """Turn the led at coordinate (x,y) on or off. (0,0) is a top left."""
