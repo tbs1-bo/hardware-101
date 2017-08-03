0ab37413bdeba55a3133a14992e0c5a027d5be48
Marco Bakera <bakera@tbs1.de>
2017-08-03 07:03:07 +0000

    code cleanup and more verbose description.

--- led_matrix/ledmatrix.py
+++ led_matrix/ledmatrix.py
@@ -32,7 +32,9 @@ import time
 class LedMatrix:
     
     def __init__(self, led_gpio, boardmode=GPIO.BCM):
-        """Create an led matrix with a configuration that maps led pins to gpio pins."""
+        """Create an led matrix with a mapping that maps led-matrix pins to
+        gpio pins."""
+        
         self.led_gpio = led_gpio
         
         self.x_pins = [1, 3, 10, 7, 8]
@@ -46,12 +48,16 @@ class LedMatrix:
             GPIO.setup(led_gpio[pin], GPIO.OUT)
 
     def led(self, x, y, on_off):
-        """Turn the led at coordinate (x,y) on or off. (0,0) is a top left."""
+        """Turn the led at coordinate (x,y) on or off. Starting with (0,0) at
+        top left. Multiple LEDs in one row/column cannot be turned on this way.
+        Use multiplexing instead - as described here
+        https://www.mikrocontroller.net/articles/LED-Matrix#Multiplexbetrieb"""
 
-        pin_hi = self.x_pins[x]
-        pin_lo = self.y_pins[y]
+        # electric current travels from column (y) to row (x) if turned on
+        pin_lo = self.x_pins[x]
+        pin_hi = self.y_pins[y]
 
-        if on_off:
+        if not on_off:
             # swap if necessary
             pin_hi, pin_lo = pin_lo, pin_hi
 
@@ -62,34 +68,28 @@ class LedMatrix:
 
 
 def main():
-    # mapping pins of the chip to GPIO-pins
-    led_gpio = {1: 17, 3: 18, 11: 23, 12: 22}
-    ledm = LedMatrix(led_gpio)
+    # mapping pins of the led-matrix to GPIO-pins
+    ledpin_gpio = {1: 17, 3: 18, 11: 23, 12: 22}
+    ledm = LedMatrix(ledpin_gpio)
 
-    # turning on each led from top left to bottom right
+    # turning on each led from top left to bottom right - for 5 seconds
     start = time.time()
     while time.time() - start < 5:
-        for x in (0, 1):
-            for y in (0, 1):
+        for y in (0, 1):
+            for x in (0, 1):
                 ledm.led(x, y, True)
                 time.sleep(0.2)
                 ledm.led(x, y, False)
                 time.sleep(0.2)
 
-    # turning on some leds using muliplexing
+    # turning on two leds at 0|0 and 0|1 using muliplexing
     while True:
         for x in (0, 1):
             for y in (0, 1):
                 if (x, y) in [(0, 0), (1, 1)]:
                     ledm.led(x, y, True)
-                    time.sleep(0.002)
+                    time.sleep(0.001)
                     ledm.led(x, y, False)
-                    
-                else:
-                    ledm.led(x, y, False)
-                    
-                #ledm.led(x, y, False)
-                #time.sleep(0.2)
 
 
 if __name__ == "__main__":
