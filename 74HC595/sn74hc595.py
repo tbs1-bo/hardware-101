03271998c01de8dc03dd6b083c45f6edf72baf82
Marco Bakera <marco@bakera.de>
2017-08-06 15:30:40 +0200

    first untested version of the 8bit shift register. (#1)

---
+++ 74HC595/sn74hc595.py
@@ -0,0 +1,76 @@
+import Rpi.GPIO as GPIO
+import time
+
+
+class SN74HC595:
+    """
+    A class that drives an 8Bit-shift register.
+
+         +--u--+
+    Qb  -|1  16|- Vcc
+    Qc  -|2  15|- Qa
+    Qd  -|3  14|- SER
+    Qe  -|4  13|- ~OE
+    Qf  -|5  12|- RCLK
+    Qg  -|6  11|- SRCLK
+    Qh  -|7  10|- ~SRCLR
+    GND -|8   9|- Qh'
+         +-----+
+    """
+
+    def __init__(self, ser_pin, rclk_pin, srclk_pin, oe_pin,
+                 clk_time=0.1, board_mode=GPIO.BCM):
+        self.ser = ser_pin
+        self.rclk = rclk_pin
+        self.srclk = srclk_pin
+        self.oe = oe_pin
+        self.clk_time = clk_time
+
+        GPIO.setmode(board_mode)
+        # all pins are output pins
+        for p in [self.ser, self.rclk, self.srclk, self.oe]:
+            GPIO.setup(p, GPIO.OUTPUT)
+
+        self.output_enable(False)
+
+    def tick_rclk(self):
+        """One tick of the clock."""
+        GPIO.output(self.rclk, GPIO.LOW)
+        time.sleep(self.clk_time)
+        GPIO.output(self.rclk, GPIO.HIGH)
+        time.sleep(self.clk_time)
+
+    def tick_srclk(self):
+        """One tick of the clock."""
+        GPIO.output(self.srclk, GPIO.LOW)
+        time.sleep(self.clk_time)
+        GPIO.output(self.srclk, GPIO.HIGH)
+        time.sleep(self.clk_time)
+
+    def output_enable(self, true_false):
+        GPIO.output(self.oe, not true_false)
+
+    def send_ser(self, signal):
+        GPIO.output(self.ser, signal)
+
+
+def main():
+    register = SN74HC595(ser_pin=4, rclk_pin=18, srclk_pin=17, oe_pin=22)
+
+    # enable the output
+    register.output_enable(True)
+
+    # send on bit into the register
+    register.srclk()
+    register.send_ser(True)
+    register.srclk()
+    register.send_ser(False)
+
+    # let the register tick some cycles
+    for i in range(5):
+        register.tick_rclk()
+        register.tick_srclk()
+
+
+if __name__ == "__main__":
+    main()
