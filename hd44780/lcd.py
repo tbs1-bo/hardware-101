6338ca62b7488fd9689d6a40d5d44ea615285ebb
Marco Bakera <marco@bakera.de>
2017-08-09 17:14:50 +0200

    init method.

--- hd44780/lcd.py
+++ hd44780/lcd.py
@@ -2,8 +2,11 @@ import RPi.GPIO as GPIO
 
 
 class LCD:
-    def __init__(self):
-        pass
+    def __init__(self, rw_pin, db_pins):
+        assert len(db_pins) == 4
+
+        self.rw_pin = rw_pin
+        self.db_pins = db_pins
 
 
 if __name__ == "__main__":
