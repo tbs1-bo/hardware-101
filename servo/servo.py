12fefb975f0d0c482e6dc9db09605d43d3dec568
Marco Bakera <marco@bakera.de>
2017-08-11 10:54:59 +0200

    more documentation in the source code.

--- servo/servo.py
+++ servo/servo.py
@@ -3,14 +3,16 @@ import time
 
 # set pin for hardware PWM
 servopin = 17
-frequency = 50  # Hz
-
+# set a frequency to be used by the servo (in Hz)
+frequency = 50
+# the pin numbering boardmode of the pi
 boardmode = GPIO.BCM
 
+# first setup the pi
 GPIO.setmode(boardmode)
 GPIO.setup(servopin, GPIO.OUT)
 
-# using creating pwm pin
+# creating pwm pin
 pwm = GPIO.PWM(servopin, frequency)
 
 print("Starting PWM")
@@ -18,10 +20,10 @@ pwm.start(2.5)
 time.sleep(3)
 
 # test different duty cycles
-for d in [3, 4, 5, 7, 9]:
-    print("duty cycle:", d)
-    pwm.ChangeDutyCycle(d)
-    time.sleep(3)
+for dc in [3, 4, 5, 7, 9]:
+    print("duty cycle:", dc)
+    pwm.ChangeDutyCycle(dc)
+    time.sleep(2)
 
 pwm.stop()
 GPIO.cleanup()
